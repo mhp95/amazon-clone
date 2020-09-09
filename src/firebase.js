a53e23615d41829ce463f73397dd15121888c17c
mhp95 <hosseinparastar95@gmail.com>
2020-09-09 13:19:03 -0300

    Making clean canvas

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAU0KJUFyp-tVc9zfICYkHBIUp2yCZFZuk",
+    authDomain: "clone-da051.firebaseapp.com",
+    databaseURL: "https://clone-da051.firebaseio.com",
+    projectId: "clone-da051",
+    storageBucket: "clone-da051.appspot.com",
+    messagingSenderId: "221476704996",
+    appId: "1:221476704996:web:19cd8b426b6bfeebbcd2f1",
+    measurementId: "G-RYHBP1C220"
+};
