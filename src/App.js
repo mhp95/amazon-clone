ed12ab07d9a6fcdbb562092e1844fa7da2bfbfb5
mhp95 <hosseinparastar95@gmail.com>
2020-09-17 22:58:03 -0300

    payment

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ import Login from "./components/Login";
 import { auth } from "./firebase";
 import { useStateValue } from "./StateProvider";
 import Payment from "./components/Payment";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+
+const promise = loadStripe(
+  "pk_test_51HSYdAF25IeuEcFcjM8HXFCDQEwMd5LFqubQaaP3N5L5wDLsELT5GoWGfhpqG7zMOziYFggw9Xg1J8dK64CINywI00E3tTxpEA"
+);
 
 function App() {
   const [{}, dispatch] = useStateValue();
@@ -42,7 +48,9 @@ function App() {
           </Route>
           <Route path="/payment">
             <Header />
-            <Payment />
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
           </Route>
           <Route path="/">
             <Header />
--- src/components/Payment.js
+++ src/components/Payment.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Payment.css";
 import CheckoutProduct from "./CheckoutProduct";
 import { useStateValue } from "../StateProvider";
 import { Link } from "react-router-dom";
+import { useStripe, useElements, CardElement } from "@stripe/react-stripe-js";
 
 function Payment() {
+  const strips = useStripe();
+  const elements = useElements();
+  const [error, setError] = useState(null);
+  const [disabled, setDisabled] = useState(null);
+  const handleSubmit = (event) => {
+    //Stripe things
+  };
+  const handleChange = (event) => {
+    setDisabled(event.empty);
+    setError(event.error ? event.error.message : "");
+  };
   const [{ basket, user }, dispatch] = useStateValue();
   return (
     <div className="payment">
@@ -42,6 +54,11 @@ function Payment() {
         <div className="payment__section">
           <div className="payment__title">
             <h1>Payment Method</h1>
+          </div>
+          <div className="payment__details">
+            <form onSubmit={handleSubmit} action="">
+              <CardElement onChange={handleChange} />
+            </form>
           </div>
         </div>
       </div>
