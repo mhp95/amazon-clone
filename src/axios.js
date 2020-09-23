612a103ba543b8714f5f2027d94a48e8ef7d148f
mhp95 <hosseinparastar95@gmail.com>
2020-09-23 03:11:16 -0300

    payment and hosting

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5001/clone-da051/us-central1/api", // The API URL,
+  baseURL: "https://us-central1-clone-da051.cloudfunctions.net/api",
+
+  //"http://localhost:5001/clone-da051/us-central1/api", // The API URL,
 });
 
 export default instance;
